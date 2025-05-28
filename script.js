f1ea3cf022f1c5a25c1a5349e82da7467121511a
i-bousabat <imedbousabat1@gmail.com>
2025-05-28 14:36:41 -0400

    Create the grid

---
+++ script.js
@@ -0,0 +1,20 @@
+let gridDimention = 16;// for example = 16
+
+const gridSize = 16; //example
+const container = document.querySelector('.container0');
+
+for (let i = 0; i < gridSize; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j = 0; j < gridSize; j++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        row.appendChild(square);
+    }
+
+    container.appendChild(row);
+}
+
+square.
+
